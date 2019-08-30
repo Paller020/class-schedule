cfcb46b7ed47849f1dc202f3ec8348dcf304d84b
Peter Aller <21016@columbushs.com>
2019-08-30 14:50:00 -0400

    First Commit

---
+++ src/classschedule/ClassSchedule.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classschedule;
+
+/**
+ *
+ * @author 21016
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+String name= "Peter Aller";
+int year= 2002;
+String course1= "Hon Printmaking";
+String teacher1= "Peter Santa-Maria";
+String course2= "AP American His";
+String teacher2= "Javier Hermida";
+String course3= "Hon English 2";
+String teacher3= "Cody Morrissey";
+String course4= "Hon Coll Alg & Trig";
+String teacher4= "Maria Acevedo";
+String course5= "AP Enviornmental Science";
+String teacher5= "Christine Taylor";
+String course6= "Hon Global Studies";
+String teacher6= "James Dugard";
+String course7= "Hon Computer Science";
+String teacher7= "Matthew Larrubia";
+        
+        
+//teacher
+System.out.println("+--------------------------------------------------------+");
+System.out.println("| 1. |           " + course1 + " |     " + teacher1 + " |");
+System.out.println("| 2. |           " + course2 + " |     " + teacher2 + "    |");
+System.out.println("| 3. |           " + course3 + "   |   "   + teacher3 + "      |");
+System.out.println("| 4. |           " + course4 + " |     " + teacher4 + "     |");
+    }
+    
+}
