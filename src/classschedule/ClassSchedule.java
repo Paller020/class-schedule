4c336c995ab96d38eea16b5c2bd86651d7228739
21016 <21016@MININT-RTINT45.columbushs.com>
2019-09-04 13:51:44 -0400

    Done

--- src/classschedule/ClassSchedule.java
+++ src/classschedule/ClassSchedule.java
@@ -15,30 +15,35 @@ public class ClassSchedule {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-String name= "Peter Aller";
-int year= 2002;
-String course1= "Hon Printmaking";
+String name = "Peter Aller";
+int year = 2021;
+String course1= "Printmaking";
 String teacher1= "Peter Santa-Maria";
 String course2= "AP American His";
 String teacher2= "Javier Hermida";
-String course3= "Hon English 2";
+String course3= "English";
 String teacher3= "Cody Morrissey";
-String course4= "Hon Coll Alg & Trig";
+String course4= "Coll Alg & Trig";
 String teacher4= "Maria Acevedo";
-String course5= "AP Enviornmental Science";
+String course5= "AP Envior. Science";
 String teacher5= "Christine Taylor";
-String course6= "Hon Global Studies";
+String course6= "Global Studies";
 String teacher6= "James Dugard";
-String course7= "Hon Computer Science";
+String course7= "Computer Science";
 String teacher7= "Matthew Larrubia";
-        
-        
+
+
+System.out.println("My name is " + name + " and I'll graduate in " + year + ".");
 //teacher
 System.out.println("+--------------------------------------------------------+");
-System.out.println("| 1. |           " + course1 + " |     " + teacher1 + " |");
-System.out.println("| 2. |           " + course2 + " |     " + teacher2 + "    |");
-System.out.println("| 3. |           " + course3 + "   |   "   + teacher3 + "      |");
-System.out.println("| 4. |           " + course4 + " |     " + teacher4 + "     |");
+System.out.println("| 1. |  " + course1 + "         |     " + teacher1 + " |");
+System.out.println("| 2. |  " + course2 + "     |     " + teacher2 + "    |");
+System.out.println("| 3. |  " + course3 + "             |   "    +  teacher3 + "      |");
+System.out.println("| 4. |  " + course4 + "     |  " + teacher4 + "        |");
+System.out.println("| 5. |  " + course5 + "  | " + teacher5 + "      |");
+System.out.println("| 6. |  " + course6 + "      | " + teacher6 + "          |");
+System.out.println("| 7. |  " + course7 + "    | " + teacher7 + "      |");
+
     }
     
 }
